e32051e91d1eff4bd82de02a16041acb6ff4bc2d
lzzy12 <jhashivam2020@gmail.com>
2020-06-15 15:49:36 +0530

    Initial support for in app handling of SCA
    Intent handling is not working currently.
    
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- lib/src/stripe.dart
+++ lib/src/stripe.dart
@@ -16,22 +16,28 @@ class Stripe {
   /// [stripeAccount] is the id of a stripe customer and stats with "cus_".
   /// This is a optional parameter.
   ///
+  /// [useWebView] is a bool wether you want to handle the SCA in a browser window or webview inside your application
+  ///
   /// [returnUrlForSca] can be used to use your own return url for
   /// Strong Customer Authentication (SCA) such as 3DS, 3DS2, BankID and others.
   /// It is recommended to use your own app specific url scheme and host.
-  Stripe(String publishableKey, {String stripeAccount, String returnUrlForSca})
+  Stripe(String publishableKey,
+      {String stripeAccount, String returnUrlForSca, bool useWebView = false})
       : api = StripeApi(publishableKey, stripeAccount: stripeAccount),
+        _useWebView = useWebView,
         _returnUrlForSca = returnUrlForSca ?? "stripesdk://3ds.stripesdk.io";
 
   final StripeApi api;
   final String _returnUrlForSca;
   static Stripe _instance;
+  bool _useWebView = false;
 
   /// Access the instance of Stripe by calling [Stripe.instance].
   /// Throws an [Exception] if [Stripe.init] hasn't been called previously.
   static Stripe get instance {
     if (_instance == null) {
-      throw Exception("Attempted to get singleton instance of Stripe without initialization");
+      throw Exception(
+          "Attempted to get singleton instance of Stripe without initialization");
     }
     return _instance;
   }
@@ -45,13 +51,17 @@ class Stripe {
   /// [stripeAccount] is the id of a stripe customer and stats with "cus_".
   /// This is a optional parameter.
   ///
+  /// [useWebView] is a bool wether you want to handle the SCA in a browser window or webview inside your application
+  /// 
   /// [returnUrlForSca] can be used to use your own return url for
   /// Strong Customer Authentication (SCA) such as 3DS, 3DS2, BankID and others.
   /// It is recommended to use your own app specific url scheme and host. This
   /// parameter must match your "android/app/src/main/AndroidManifest.xml"
   /// and "ios/Runner/Info.plist" configuration.
-  static void init(String publishableKey, {String stripeAccount, String returnUrlForSca}) {
-    _instance = Stripe(publishableKey, stripeAccount: stripeAccount, returnUrlForSca: returnUrlForSca);
+  static void init(String publishableKey,
+      {String stripeAccount, String returnUrlForSca, bool useWebView = false}) {
+    _instance = Stripe(publishableKey,
+        stripeAccount: stripeAccount, returnUrlForSca: returnUrlForSca, useWebView: useWebView);
     StripeApi.init(publishableKey, stripeAccount: stripeAccount);
   }
 
@@ -62,7 +72,8 @@ class Stripe {
     return "$_returnUrlForSca?requestId=$requestId";
   }
 
-  @Deprecated("Use `Stripe.instance.getReturnUrlForSca()` instead. Will be removed in v3.0.")
+  @Deprecated(
+      "Use `Stripe.instance.getReturnUrlForSca()` instead. Will be removed in v3.0.")
   static String getReturnUrl() {
     final requestId = Random.secure().nextInt(99999999);
     return "stripesdk://3ds.stripesdk.io?requestId=$requestId";
@@ -71,7 +82,8 @@ class Stripe {
   /// Confirm a SetupIntent
   /// https://stripe.com/docs/api/setup_intents/confirm
   Future<Map<String, dynamic>> confirmSetupIntent(String clientSecret) async {
-    final intent = await api.confirmSetupIntent(clientSecret, data: {'return_url': getReturnUrlForSca()});
+    final intent = await api.confirmSetupIntent(clientSecret,
+        data: {'return_url': getReturnUrlForSca()});
     if (intent['status'] == 'requires_action') {
       // ignore: deprecated_member_use_from_same_package
       return handleSetupIntent(intent['next_action']);
@@ -82,9 +94,12 @@ class Stripe {
 
   /// Confirm a SetupIntent with a PaymentMethod
   /// https://stripe.com/docs/api/setup_intents/confirm
-  Future<Map<String, dynamic>> confirmSetupIntentWithPaymentMethod(String clientSecret, String paymentMethod) async {
-    final intent = await api
-        .confirmSetupIntent(clientSecret, data: {'return_url': getReturnUrlForSca(), 'payment_method': paymentMethod});
+  Future<Map<String, dynamic>> confirmSetupIntentWithPaymentMethod(
+      String clientSecret, String paymentMethod) async {
+    final intent = await api.confirmSetupIntent(clientSecret, data: {
+      'return_url': getReturnUrlForSca(),
+      'payment_method': paymentMethod
+    });
     if (intent['status'] == 'requires_action') {
       // ignore: deprecated_member_use_from_same_package
       return handleSetupIntent(intent['next_action']);
@@ -96,10 +111,12 @@ class Stripe {
   /// Confirm and authenticate a payment.
   /// Returns the PaymentIntent.
   /// https://stripe.com/docs/payments/payment-intents/android
-  Future<Map<String, dynamic>> confirmPayment(String paymentIntentClientSecret, {String paymentMethodId}) async {
+  Future<Map<String, dynamic>> confirmPayment(String paymentIntentClientSecret,
+      {String paymentMethodId}) async {
     final data = {'return_url': getReturnUrlForSca()};
     if (paymentMethodId != null) data["payment_method"] = paymentMethodId;
-    final paymentIntent = await api.confirmPaymentIntent(paymentIntentClientSecret, data: data);
+    final paymentIntent =
+        await api.confirmPaymentIntent(paymentIntentClientSecret, data: data);
     if (paymentIntent['status'] == "requires_action") {
       // ignore: deprecated_member_use_from_same_package
       return handlePaymentIntent(paymentIntent['next_action']);
@@ -111,9 +128,12 @@ class Stripe {
   /// Authenticate a payment.
   /// Returns the PaymentIntent.
   /// https://stripe.com/docs/payments/payment-intents/android-manual
-  Future<Map<String, dynamic>> authenticatePayment(String paymentIntentClientSecret) async {
-    final paymentIntent = await api.retrievePaymentIntent(paymentIntentClientSecret);
-    if (paymentIntent['status'] != "requires_action") return Future.value(paymentIntent);
+  Future<Map<String, dynamic>> authenticatePayment(
+      String paymentIntentClientSecret) async {
+    final paymentIntent =
+        await api.retrievePaymentIntent(paymentIntentClientSecret);
+    if (paymentIntent['status'] != "requires_action")
+      return Future.value(paymentIntent);
     final nextAction = paymentIntent['next_action'];
     // ignore: deprecated_member_use_from_same_package
     return handlePaymentIntent(nextAction);
@@ -123,14 +143,16 @@ class Stripe {
   /// This is similar to [authenticatePayment] but is slightly more efficient,
   /// as it avoids the request to the Stripe API to retrieve the action.
   /// To use this, return the complete [nextAction] from your server.
-  Future<Map<String, dynamic>> authenticatePaymentWithNextAction(Map nextAction) async {
+  Future<Map<String, dynamic>> authenticatePaymentWithNextAction(
+      Map nextAction) async {
     // ignore: deprecated_member_use_from_same_package
     return handlePaymentIntent(nextAction);
   }
 
   /// Launch 3DS in a new browser window.
   /// Returns a [Future] with the Stripe PaymentIntent when the user completes or cancels authentication.
-  @Deprecated("Use [authenticatePaymentWithNextAction] instead. Will be removed in v3.0.")
+  @Deprecated(
+      "Use [authenticatePaymentWithNextAction] instead. Will be removed in v3.0.")
   Future<Map<String, dynamic>> handlePaymentIntent(Map action) async {
     return _authenticateIntent(
         action,
@@ -151,7 +173,8 @@ class Stripe {
             ));
   }
 
-  Future<Map<String, dynamic>> _authenticateIntent(Map action, IntentProvider callback) async {
+  Future<Map<String, dynamic>> _authenticateIntent(
+      Map action, IntentProvider callback) async {
     final url = action['redirect_to_url']['url'];
     final returnUrl = Uri.parse(action['redirect_to_url']['return_url']);
     final completer = Completer<Map<String, dynamic>>();
@@ -159,14 +182,15 @@ class Stripe {
     sub = getUriLinksStream().listen((Uri uri) async {
       if (uri.scheme == returnUrl.scheme &&
           uri.host == returnUrl.host &&
-          uri.queryParameters['requestId'] == returnUrl.queryParameters['requestId']) {
+          uri.queryParameters['requestId'] ==
+              returnUrl.queryParameters['requestId']) {
         await sub.cancel();
         final intent = await callback(uri);
         completer.complete(intent);
       }
     });
 
-    await launch(url);
+    await launch(url, forceWebView: _useWebView, enableJavaScript: true);
     return completer.future;
   }
 }
